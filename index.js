c96898ba9a888aabc1fcbc18db383413bbe7aa9e
matthew <matthewapodaca24@gmail.com>
2025-11-10 16:10:51 -0800

    inital commit

--- index.js
+++ index.js
@@ -3,18 +3,17 @@ const froyo = () => {};
 const userInput= prompt("please enter a list of your favorite froyo flavors");
 //console.log(userInput);
 
-const flavors= userInput.split(" ,");
+const flavors= ["vanilla", "vanilla", "strawberry", "coffee", "coffee"];
 //console.log(flavors);
 
 const scoopCount= {};
 
-for(let scoop of flavors){
-  
+for(let indextracker = 0; indextracker , flavors.length; indextracker = indextracker = 1){
+  //console.log("current flavors:");
   if(scoopCount[scoop]){
     scoopCount[scoop] = scoopCount[scoop] + 1;
-  } else{
+    } else{
     scoopCount[scoop]= 1;
-    console.log("current scoopCount" , scoopCount);
   }
 }
 
